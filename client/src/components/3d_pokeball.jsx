ce16836ca3cbe2d38ea60e4ec54214995c8ebfc1
rahul baghel <rahulbaghelp01@gmail.com>
2026-08-06 00:01:21 +0530

    Added needed library for the mdal to show

---
+++ client/src/components/3d_pokeball.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { Center, useGLTF } from "@react-three/drei";
+import pokeballModel from "../assets/Pokeball_textured.glb?url";
+
+export default function Model() {
+  const model = useGLTF(pokeballModel);
+
+  useEffect(() => {
+    console.log(model);
+  }, [model]);
+
+  return (
+    <Center>
+      <primitive object={model.scene} scale={0.1} />
+    </Center>
+  );
+}
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,13 +1,25 @@
-function Home(){
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Model from "../components/3d_pokeball";
 
+function Home() {
+  return (
+    <main className="min-h-screen w-screen flex items-center justify-center bg-[var(--black)]">
+      <div className="w-full h-[90vh] max-w-[900px]">
+        <Canvas
+          className="block h-full w-full"
+          camera={{ position: [0, 0, 7], fov: 45, near: 0.1, far: 1000 }}
+          dpr={[1, 2]}
+        >
+          <ambientLight intensity={1.2} />
+          <directionalLight position={[5, 5, 5]} intensity={3} />
+          <Suspense fallback={null}>
+            <Model />
+          </Suspense>
+        </Canvas>
+      </div>
+    </main>
+  );
+}
 
-
-    return (
-        <main className="">
-
-        </main>
-    )
-} 
-
-
-export default Home 
+export default Home;
