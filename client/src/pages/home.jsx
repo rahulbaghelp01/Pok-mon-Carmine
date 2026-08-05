eed54394bc40ba43bc7e5129d6bd6e0be969519b
rahul baghel <rahulbaghelp01@gmail.com>
2026-08-06 00:03:43 +0530

    added the modal successfully

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,5 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
+import { OrbitControls } from "@react-three/drei";
 import Model from "../components/3d_pokeball";
 
 function Home() {
@@ -8,14 +9,16 @@ function Home() {
       <div className="w-full h-[90vh] max-w-[900px]">
         <Canvas
           className="block h-full w-full"
-          camera={{ position: [0, 0, 10], fov: 35, near: 0.1, far: 1000 }}
+          camera={{ position: [0, 0, 8], fov: 40, near: 0.1, far: 1000 }}
           dpr={[1, 2]}
         >
+          <color attach="background" args={["#161311"]} />
           <ambientLight intensity={1.2} />
           <directionalLight position={[5, 5, 5]} intensity={3} />
           <Suspense fallback={null}>
             <Model />
           </Suspense>
+          <OrbitControls enablePan={false} enableZoom={true} />
         </Canvas>
       </div>
     </main>
