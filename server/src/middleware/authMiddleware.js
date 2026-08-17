3d43c470f7788bb7b578c55437fbe54ff35c1b40
rahul baghel <rahulbaghelp01@gmail.com>
2026-08-17 19:13:25 +0530

    Created the middle ware and the finished up pokemons route for the auth apge

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -4,12 +4,24 @@ const authMiddleware = (req, res, next) => {
     const { authorization } = req.headers;
 
     if (!authorization) {
-        return res.status(401).json({ error: "Authorization token missing" });
+        return res.status(401).json({
+            error: "Authorization token missing"
+        });
     }
 
     const token = authorization.split(" ")[1];
 
-    jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{});
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+        next();
+
+    } catch (err) {
+        return res.status(401).json({
+            error: "Invalid token"
+        });
+    }
 };
 
 export default authMiddleware;
