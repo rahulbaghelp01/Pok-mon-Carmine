184aac055f6a2203bab481a43561fea8b588844c
rahul baghel <rahulbaghelp01@gmail.com>
2026-08-18 14:27:07 +0530

    Created the final route for the /auth page ( login route )  and finished the backend work for the  (/auth) page

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -6,16 +6,17 @@ import "dotenv/config";
 import pkg from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { customAlphabet } from "nanoid";
+import authMiddleware from "../middleware/authMiddleware";
 
 
 const { PrismaClient } = pkg;
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL
 });
 
 const prisma = new PrismaClient({
-  adapter
+    adapter
 });
 
 const router = express.Router();
@@ -24,19 +25,28 @@ const router = express.Router();
 // GET request for starter Pokemons
 
 router.get("/", async (req, res) => {
-  const pokemons = await prisma.pokemon.findMany({
-    where: {
-      id: {
-        in: [1, 4, 7]
-      }
-    }
-  });
+    const pokemons = await prisma.pokemon.findMany({
+        where: {
+            id: {
+                in: [1, 4, 7]
+            }
+        }
+    });
 
-  res.json(pokemons);
+    res.json(pokemons);
 });
 
 
 // POST request for validating the token
+router.post("/validate", authMiddleware, (req, res) => {
+    res.status(200).json({
+        message: "User authenticated",
+        user: req.user
+    });
+});
+
+
+// POST request for registering a new user
 
 router.post("/register", async (req, res) => {
     const { username, email, password, pokemonId } = req.body;
@@ -99,7 +109,48 @@ router.post("/register", async (req, res) => {
 
 // POST request for logging in
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
+
+    const { identifier, password } = req.body;
+
+    const user = await prisma.user.findFirst({
+        where: {
+            OR: [
+                { email: identifier },
+                { username: identifier }
+            ]
+        }
+    })
+
+    if (!user) {
+        return res.status(401).json({
+            message: "Invalid credentials. Please check your username/email and password."
+        })
+    }
+
+    const isValidpassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidpassword) {
+        return res.status(401).json({
+            message: "Invalid credentials. Please check your username/email and password."
+        })
+    }
+
+    const token = jwt.sign({
+        userId: user.id,
+        gamingId: user.gamingId,
+        username: user.username
+    },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d"
+        });
+
+    res.status(200).json({
+        message: "Login successful",
+        token
+    });
+
 
 });
 
